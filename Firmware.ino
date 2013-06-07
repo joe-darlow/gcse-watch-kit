184b5f30aa82918056d18055c80472ff9c330590
joe-darlow <joedarlow@outlook.com>
2013-06-07 22:57:11 +0200

    Update Firmware.ino

--- Firmware.ino
+++ Firmware.ino
@@ -25,7 +25,7 @@
  
  We turn off Brown out detect because it alone uses ~16uA.
  
- */
+*/
 
 #include <avr/sleep.h> //Needed for sleep_mode
 #include <avr/power.h> //Needed for powering down perihperals such as the ADC/TWI and Timers
